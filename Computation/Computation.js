f97a0dfe5371fa8c4ab6228fe0d84bc48a393b6a
Jignesh Trivedi <jignesht024@gmail.com>
2020-04-03 09:27:49 +0530

    try with npm dependencies

--- Computation/Computation.js
+++ Computation/Computation.js
@@ -1,11 +1,12 @@
 module.exports = function(RED) {
+	var addNumbers = require('add-numbers');
 	function ComputationNode(config) {
 		RED.nodes.createNode (this,config);
 		var node= this;
 		this.input1= config.input1;
 		this.input2= config.input2;
 		node.on('input', function(msg) {
-			msg.payload = Number(this.input1) + Number(this.input2);
+			msg.payload = addNumbers(Number(this.input1), Number(this.input2));
 			node.send(msg);
 		});
 	}
